208f5f92f93c3a7926e1e8e03420a56c772d1170
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 22:46:00 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+    const firstName = 'Chairat';
+    let lastName = 'Sapsanong';
+    var nickname = 'Toon';
+    firstName = 'Smith';
+    lastName = 'Loving'
+    nickname = 'Smitty';
+    birthDate = '1992-03-15';
